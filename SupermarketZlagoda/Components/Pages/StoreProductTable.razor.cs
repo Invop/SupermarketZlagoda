856f7bb554849b56ef2d8fda187ea1231af75099
Invop <shandrykjj512@gmail.com>
2024-02-26 21:18:05 +0200

    Add StoreProductTable component with sorting feature
    Implemented a new StoreProductTable component for product display in SupermarketZlagoda along with enhancements in product sorting options. The new component, its corresponding C# logic file, and a new StoreProduct data model were added, offering greater functionality and accuracy in product data management. Resultant features comprise of dynamic sorting by product name or quantity and product-specific filters for managers.

---
+++ SupermarketZlagoda/Components/Pages/StoreProductTable.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using SupermarketZlagoda.Data;
+using SupermarketZlagoda.Data.Model;
+
+namespace SupermarketZlagoda.Components.Pages;
+
+public partial class StoreProductTable
+{
+    private bool IsManager { get; set; } = false;
+    
+    private string _searchTerm = string.Empty;
+    private int _sortType = 0;
+    private bool _hidePromotional = false, _hideNonPromotional = false;
+    private readonly PaginationState _pagination = new() { ItemsPerPage = 20 };
+    private IQueryable<StoreProduct>? _items = Enumerable.Empty<StoreProduct>().AsQueryable();
+    private List<SelectOption> CategoryOptions = new List<SelectOption>()
+    {
+        new() { Value = "1", Text = "Technology", Selected = true },
+        new() { Value = "2", Text = "Science", Selected = true },
+        new() { Value = "3", Text = "Art & Culture" },
+        new() { Value = "4", Text = "Health & Wellness" },
+        new() { Value = "5", Text = "Sports" },
+        new() { Value = "6", Text = "Education" },
+        // ... other category options
+    };
+    
+    protected override Task OnInitializedAsync()
+    {
+        IsManager = UserState.IsManager;
+        return base.OnInitializedAsync();
+    }
+    
+    private void HandleSelectCategoryChange(List<SelectOption> selectedOptions)
+    {
+        foreach (var selectedOption in selectedOptions.Where(selectedOption => selectedOption.Selected))
+        {
+            Console.WriteLine(selectedOption.Value);
+        }
+    }
+
+    private IQueryable<StoreProduct> GenerateSampleGridData(int size)
+    {
+        StoreProduct[] data = new StoreProduct[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = new StoreProduct(
+                "123456789012",
+                "123456789012",
+                i,
+                100.00M,
+                100,
+                i % 2 == 0);
+        }
+        return data.AsQueryable();
+    }
+    protected override void OnInitialized()
+    {
+        _items = GenerateSampleGridData(5000);
+    }
+}
