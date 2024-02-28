20345d7b0bcb53155ad2d4ed2ee2c1598becf0a9
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-02-28 22:41:17 +0200

    Add Customer Card table

---
+++ SupermarketZlagoda/Components/Pages/CustomerCardTable.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using SupermarketZlagoda.Data;
+using SupermarketZlagoda.Data.Model;
+
+namespace SupermarketZlagoda.Components.Pages;
+public partial class CustomerCardTable
+{
+    private bool IsManager { get; set; } = false;
+    private string _surnameSearchTerm = string.Empty;
+    private int _sortType = 0;
+    
+    private string? percentageValue;
+    Option<int>? selectedPercentageOption;
+    
+    private readonly PaginationState _pagination = new() { ItemsPerPage = 20 };
+    private IQueryable<CustomerCard>? _items = Enumerable.Empty<CustomerCard>().AsQueryable();
+    static List<Option<int>> PercentageOptions = new()
+    {
+        { new Option<int> { Value = 10, Text = 10 } },
+        { new Option<int> { Value = 20, Text = 20 } },
+        { new Option<int> { Value = 30, Text = 30 } },
+        { new Option<int> { Value = 90, Text = 90 } },
+        
+    };
+    protected override Task OnInitializedAsync()
+    {
+        IsManager = UserState.IsManager;
+        return base.OnInitializedAsync();
+    }
+    
+    private void HandleSelectPercentageChange(List<SelectOption> selectedOptions)
+    {
+        foreach (var selectedOption in selectedOptions.Where(selectedOption => selectedOption.Selected))
+        {
+            Console.WriteLine(selectedOption.Value);
+        }
+    }
+
+    private IQueryable<CustomerCard> GenerateSampleGridData(int size)
+    {
+        CustomerCard[] data = new CustomerCard[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = new CustomerCard($"{i}", $"surname{i}", $"name{i}", $"patronymic{i}",
+                "+380000000000", $"city nameeeeeeee{i}", $"street nameeeeee {i}",$"396{i}", 10);
+        }
+        return data.AsQueryable();
+    }
+    protected override void OnInitialized()
+    {
+        _items = GenerateSampleGridData(5000);
+    }
+}
---
+++ SupermarketZlagoda/Data/Model/CustomerCard.cs
@@ -0,0 +1,4 @@
+﻿namespace SupermarketZlagoda.Data.Model;
+
+public record CustomerCard(string NumberCardCustomer, string CustomerSurname, string CustomerName, string CustomerPatronymic, 
+string CustomerPhone, string CustomerCity, string CustomerStreet, string CustomerIndex, int CustomerPercentage);
