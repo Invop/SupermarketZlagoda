1d037d526f5602e7d3dd61d75b828b1dfc9d4610
Invop <shandrykjj512@gmail.com>
2024-03-20 16:04:07 +0200

    Update API endpoints and remove unused file

--- Zlagoda.Api/ApiEndpoints.cs
+++ Zlagoda.Api/ApiEndpoints.cs
@@ -7,7 +7,7 @@ public class ApiEndpoints
     {
         private const string Base = $"{ApiBase}/products";
         public const string Create = Base;
-        public const string Get = $"{Base}/{{idOrSlug}}";
+        public const string Get = $"{Base}/{{id:guid}}";
         public const string GetAll = Base;
         public const string Update = $"{Base}/{{id:guid}}";
         public const string Delete = $"{Base}/{{id:guid}}";
