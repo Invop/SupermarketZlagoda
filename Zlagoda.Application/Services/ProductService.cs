b142cdccf0cbac4e78cc9366bfd6bfe3a7878669
Invop <shandrykjj512@gmail.com>
2024-03-20 21:55:28 +0200

    Initialize ProductRepository in ProductService constructor fix.

--- Zlagoda.Application/Services/ProductService.cs
+++ Zlagoda.Application/Services/ProductService.cs
@@ -6,6 +6,11 @@ namespace Zlagoda.Application.Services;
 public class ProductService : IProductService
 {
     private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
     
     public async Task<bool> CreateAsync(Product product)
     {
