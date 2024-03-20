cd01569d6c450ba7760d01a508ad1642f2e63326
Invop <shandrykjj512@gmail.com>
2024-03-20 15:37:30 +0200

    Add product handling features
    Added new functionalities for handling products. This includes the creation of product request and response contracts, repository interfaces and implementation, service collection extensions for dependency injection and product controller for API exposure. Also adjusted the endpoint routing accordingly.

---
+++ Zlagoda.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,49 @@
+﻿using Zlagoda.Application.Models;
+using Zlagoda.Contracts.Requests;
+using Zlagoda.Contracts.Responses;
+
+namespace Zlagoda.Api.Mapping;
+
+public static class ContractMapping
+{
+    public static Product MapToProduct(this CreateProductRequest request)
+    {
+        return new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            CategoryId = request.CategoryId,
+            Characteristics = request.Characteristics
+        };
+    }
+
+    public static ProductResponse MapToResponse(this Product movie)
+    {
+        return new ProductResponse()
+        {
+            Id = movie.Id,
+            Name = movie.Name,
+            CategoryId = movie.CategoryId,
+            Characteristics = movie.Characteristics
+        };
+    }
+    
+    public static ProductsResponse MapToResponse(this IEnumerable<Product> movies)
+    {
+        return new ProductsResponse
+        {
+            Items = movies.Select(MapToResponse)
+        };
+    }
+
+    public static Product MapToProduct(this UpdateProductRequest request,Guid id)
+    {
+        return new Product
+        {   
+            Id = id,
+            Name = request.Name,
+            CategoryId = request.CategoryId,
+            Characteristics = request.Characteristics
+        };
+    }
+}
---
+++ Zlagoda.Application/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Zlagoda.Application.Models;
+
+namespace Zlagoda.Application.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    public async Task<bool> CreateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ExistsByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
