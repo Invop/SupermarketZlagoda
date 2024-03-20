6bbc8de0803144eb29aef2f118a04d1757cdae8a
Invop <shandrykjj512@gmail.com>
2024-03-20 13:40:56 +0200

    Add API, Application, and Contracts projects
    Added three new projects- API, Application, and Contracts, to the Zlagoda solution. API project includes the setup for a Swagger API and the Application project provides the service extensions and database setup. Contracts project is also added as a placeholder for future implementations.

---
+++ Zlagoda.Api/ApiEndpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace Zlagoda.Api;
+
+public class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+}
---
+++ Zlagoda.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace Zlagoda.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {   
+        
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services,string connectionString)
+    {
+        // services.AddSingleton<IDbConnectionFactory>(_ =>
+        //     new NpgsqlConnectionFactory(connectionString));
+        // services.AddSingleton<DbInitializer>();
+        return services;
+    }
+}
