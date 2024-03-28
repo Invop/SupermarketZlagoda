87560f2aa5e44dbc0ea116da17705e20172f90be
Invop <shandrykjj512@gmail.com>
2024-03-28 23:01:25 +0200

    endpoint fix

--- Zlagoda.Api/ApiEndpoints.cs
+++ Zlagoda.Api/ApiEndpoints.cs
@@ -34,14 +34,4 @@ public class ApiEndpoints
         public const string Delete = $"{Base}/{{id:guid}}";
     }
     
-    public static class CustomerCards
-    {
-        private const string Base = $"{ApiBase}/customer-card";
-        public const string Create = Base;
-        public const string Get = $"{Base}/{{id:guid}}";
-        public const string GetAll = Base;
-        public const string Update = $"{Base}/{{id:guid}}";
-        public const string Delete = $"{Base}/{{id:guid}}";
-    }
-    
 }
