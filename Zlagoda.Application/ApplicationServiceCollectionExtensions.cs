979c98e003ec939427fcdf0db31336d51f85f33a
Invop <shandrykjj512@gmail.com>
2024-03-29 23:19:48 +0200

    services resolve confl

--- Zlagoda.Application/ApplicationServiceCollectionExtensions.cs
+++ Zlagoda.Application/ApplicationServiceCollectionExtensions.cs
@@ -11,6 +11,8 @@ public static class ApplicationServiceCollectionExtensions
     {
         services.AddSingleton<IProductRepository, ProductRepository>();
         services.AddSingleton<IProductService, ProductService>();
+        services.AddSingleton<IStoreProductRepository, StoreProductRepository>();
+        services.AddSingleton<IStoreProductService, StoreProductService>();
         services.AddSingleton<ICustomerCardRepository, CustomerCardRepository>();
         services.AddSingleton<ICustomerCardService, CustomerCardService>();
         services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
