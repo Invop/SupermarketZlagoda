b3da4acfd435eef390bb5243b1907f3939cc5506
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-04-20 16:12:29 +0300

    Employee adding/editing dialog restrictions

--- SupermarketZlagoda/Data/Model/Employee.cs
+++ SupermarketZlagoda/Data/Model/Employee.cs
@@ -1,17 +1,52 @@
-﻿namespace SupermarketZlagoda.Data.Model;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketZlagoda.Data.Model;
 
 public record Employee
 {
     public Guid Id { get; init; }
+    
+    [Required]
+    [MinLength(3, ErrorMessage = "Surname is too short!")]
+    [StringLength(50, ErrorMessage = "Surname is too long (50 characters limit).")]
     public string Surname { get; set; }
+    
+    [Required]
+    [MinLength(3, ErrorMessage = "Name is too short!")]
+    [MaxLength(50, ErrorMessage = "Name is too long (50 characters limit).")]
     public string Name { get; set; }
+    
+    [MaxLength(50, ErrorMessage = "Patronymic is too long (50 characters limit).")]
     public string Patronymic { get; set; }
+    
+    [Required]
     public string Role { get; set; }
+
+    [Required]
+    [Range(0, double.MaxValue, ErrorMessage = "Salary cannot be negative.")]
     public decimal Salary { get; set; }
+    
+    [Required]
     public DateOnly DateOfStart { get; set; }
+    
+    [Required]
+    [MinimumAge(18, ErrorMessage = "Employee must be at least 18 years old.")]
     public DateOnly DateOfBirth { get; set; }
+    
+    [Required]
+    [MaxLength(13, ErrorMessage = "Phone number is too long (13 characters limit).")]
     public string PhoneNumber { get; set; }
+    
+    [Required]
+    [MaxLength(50, ErrorMessage = "City is too long (50 characters limit).")]
     public string City { get; set; }
+    
+    [Required]
+    [MaxLength(50, ErrorMessage = "Street is too long (50 characters limit).")]
     public string Street { get; set; }
+    
+    [Required]
+    [MaxLength(9, ErrorMessage = "ZipCode is too long (9 characters limit).")]
     public string ZipCode { get; set; }
 }
---
+++ SupermarketZlagoda/Data/Model/MinimumAgeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketZlagoda.Data.Model
+{
+    public class MinimumAgeAttribute(int minimumAge) : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is not DateOnly dateOfBirth) return false;
+            return DateOnly.FromDateTime(DateTime.Today.AddYears(-minimumAge)) >= dateOfBirth;
+        }
+        
+    }
+}
