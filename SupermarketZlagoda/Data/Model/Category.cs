d1cd95e1dd94f4c050c786719c1f23c4da93682d
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-04-20 16:28:17 +0300

    Category adding/editing dialog restrictions

--- SupermarketZlagoda/Data/Model/Category.cs
+++ SupermarketZlagoda/Data/Model/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace SupermarketZlagoda.Data.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketZlagoda.Data.Model;
 
 public record Category
 {
     public Guid Id { get; init; }
+    [Required]
+    [MinLength(3, ErrorMessage = "Name is too short!")]
+    [MaxLength(50, ErrorMessage = "Name is too long (50 characters limit).")]
     public string Name { get; set; }
 }
