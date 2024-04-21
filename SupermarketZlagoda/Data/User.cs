39f075c147a223b41680d9a2f4daded53b1d75cf
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-04-22 00:24:50 +0300

    Fix menu

--- SupermarketZlagoda/Data/User.cs
+++ SupermarketZlagoda/Data/User.cs
@@ -9,5 +9,5 @@ public static class User
     public static Employee? Data { get; set; } = null;
 
     public static bool Authorized { get; set; } = false;
-    public static bool IsManager { get => Authorized && Data.Role.Equals("Manager"); }
+    public static bool IsManager => Authorized && Data.Role.Equals("Manager");
 }
